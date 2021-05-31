a349f0d4a69e6f58f1ca203005f4437db6aa60b3
Martirosyan Ruben <mruben93@gmail.com>
2021-05-31 13:04:18 +0400

    Implemented Enum validation by source generator

---
+++ TestSourceGen/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TestSourceGen
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            EnumValidation.EnumValidator.Validate((Example1)7);
+            EnumValidation.EnumValidator.Validate((Example2)7);
+        }
+    }
+
+    internal enum Example1
+    {
+        First = 1,
+        Second = 2
+    }
+
+    internal enum Example2
+    {
+        First = 7,
+        Second = 11,
+        Third = 1
+    }
+}
